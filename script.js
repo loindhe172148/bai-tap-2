5ca4b6c0987aa97d8bcfdfc7e0df005fe6e9d5f7
Nguyen Dong Loi <ndl300803@gmail.com>
2024-09-08 22:00:00 +0700

    up

---
+++ script.js
@@ -0,0 +1,18 @@
+function scrollToTop() {
+  var element = document.getElementById("slider");
+  element.scrollIntoView({ behavior: "smooth" });
+}
+document.querySelector(".menu-toggle").addEventListener("click", function () {
+  const menu = document.querySelector(".menu");
+  menu.style.display =
+    menu.style.display === "none" || menu.style.display === ""
+      ? "flex"
+      : "none";
+});
+document.querySelector(".menu-toggle").addEventListener("click", function () {
+  const menu = document.querySelector(".contact_button");
+  menu.style.display =
+    menu.style.display === "none" || menu.style.display === ""
+      ? "flex"
+      : "none";
+});
